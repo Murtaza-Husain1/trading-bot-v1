1bad4a584b752d97e0e62689451ce0e1533d2e7e
Murtaza <Murch786@gmail.com>
2019-11-25 01:03:20 -0500

    top and bottom frame

--- ui.py
+++ ui.py
@@ -5,8 +5,13 @@ SIZE="500x500"
 app = gui("Trading Bot V"+VERSION, SIZE)
 
 app.startFrame("TOP", row=0, column=0)
+app.addLabel("TOP")
+app.setLabelBg("TOP", "green")
+app.stopFrame()
 
-
+app.startFrame("BOTTOM", row=1, column=0)
+app.addLabel("BOTTOM")
+app.setLabelBg("BOTTOM", "blue")
 app.stopFrame()
 
 app.go()
